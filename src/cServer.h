b24f1af22c12b574bb939f4755289604ae03ad94
Arteiimis <lionelym0987@gmail.com>
2024-04-22 23:22:09 +0800

    init branch

---
+++ src/cServer.h
@@ -0,0 +1,28 @@
+#ifndef CSERVER_H
+#define CSERVER_H
+
+#include "boost/asio/io_context.hpp"
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+#include <boost/beast/http.hpp>
+
+namespace beast = boost::beast;
+namespace http  = beast::http;
+namespace net   = boost::asio;
+using tcp       = boost::asio::ip::tcp;
+
+class cServer: public std::enable_shared_from_this<cServer>
+{
+    using port_t = unsigned short;
+
+public:
+    cServer(net::io_context& ioc, port_t port);
+    void start();
+
+private:
+    tcp::acceptor    _acceptor;  // 用于接受客户端连接的监听器
+    net::io_context& _ioc;       // 用于处理客户端连接的io_context
+    tcp::socket      _socket;    // 用于处理客户端连接的socket
+};
+
+#endif  // !CSERVER_H
